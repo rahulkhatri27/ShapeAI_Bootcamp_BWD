4b3fac0fc4aad4b26b4cf67a4101094bbc651a76
rahulkhatri27 <84219341+rahulkhatri27@users.noreply.github.com>
2021-06-27 11:07:09 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js </h1>
+      <p>
+        This was an amazing bootcamp taken up by Shaurya Sinha we converd
+        everything form Scretch including JavaScript React.js Html.
+      </p>
+    </div>
+  );
+}
+
+export default Info;
